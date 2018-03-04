0054d90acf58e8c6ccfb7887921e1ebf1833e03e
Mechasparrow <mikipux7@gmail.com>
2018-03-03 22:56:07 -0600

    created simple potential view for user viewing textbooks and their offers

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 //Import the API
 import {TextbookTradeSystemApi} from "../../lib/TTS_Api";
 
+//Import the models
+import {Textbook} from '../../models/textbook';
+import {Offer} from '../../models/offer';
+
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -11,16 +15,43 @@ import {TextbookTradeSystemApi} from "../../lib/TTS_Api";
 })
 export class ProfilePageComponent implements OnInit {
 
+  public user_offers:Offer[] = [];
+  public user_textbooks:Textbook[] = [];
+
   constructor(
     private api: TextbookTradeSystemApi
   ) { }
 
   ngOnInit() {
+
+    let that = this;
+
+    //Retrieve the user created offers
+    this.api.getUserOffers().then (function (offers:Offer[]) {
+      that.user_offers = offers
+      console.log(that.user_offers);
+    }).catch (function (err) {
+      console.log(err);
+    });
+
+    //Retrieve the user created textbooks
+    this.api.getUserTextbooks().then (function (textbooks: Textbook[]) {
+      that.user_textbooks = textbooks;
+      console.log(that.user_textbooks);
+    }).catch (function (err) {
+      console.log(err);
+    })
+
+  }
+
+  getOfferTextbook(offer:Offer) {
+    
+    return this.user_textbooks.find(function (textbook:Textbook) {
+      return offer.textbook_id == textbook.id
+    })
   }
 
   getTest() {
-
-    this.api.getUserOffers();
 
   }
 
