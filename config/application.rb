30d0ebd4cd4e0d4bd705ef8d4d415971e69dd38e
Mechasparrow <mikipux7@gmail.com>
2018-03-03 21:53:09 -0600

    changed to api application (i think)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module TextbookProblemBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.api_only = true
+
+    config.debug_exception_response_format = :api
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
