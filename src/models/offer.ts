92aa5d2fc6d5ef2c12675f2a3b9ebb235ba415b5
Mechasparrow <mikipux7@gmail.com>
2018-03-18 15:22:06 -0500

    modified offer and textbook forms

--- src/models/offer.ts
+++ src/models/offer.ts
@@ -4,9 +4,12 @@ export class Offer {
   textbook_id:number
   textbook_name:string
   price:number
+  is_public:boolean
+  lat:number
+  lon:number
 
   static createEmptyOffer () {
-    return <Offer>{textbook_name: "", price: 0}
+    return <Offer>{textbook_id: null, price: 0.0, lat: 0, lon: 0, is_public: true}
   }
 
 }
--- src/models/textbook.ts
+++ src/models/textbook.ts
@@ -2,10 +2,15 @@ export class Textbook {
 
   id:number
   name:string
-  description:string
+
+  book_id:number
+  user_id:number
+  status:string
+  owner_description:string
+  is_public:boolean
 
   static createEmptyTextbook() {
-      return <Textbook> {name: "", description: ""}
+      return <Textbook> {book_id: null, user_id: null, status: "", owner_description: "", is_public: true}
   }
 
 }
