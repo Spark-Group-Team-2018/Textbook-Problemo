a8dc2d5864cbe0e8e16a52fc3ae0457cbaff93ae
Mechasparrow <mikipux7@gmail.com>
2018-03-22 22:09:11 -0500

    added ability to pending offer

--- src/app/browse-offers-page/browse-offers-page.component.ts
+++ src/app/browse-offers-page/browse-offers-page.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 //models
 import {Offer} from '../../models/offer';
 import {Textbook} from '../../models/textbook';
+import {PendingOffer} from '../../models/pendingoffer';
 
 //TTS Api
 import {TextbookTradeSystemApi} from '../../lib/TTS_Api';
+
+//Routing System Lib
+import { RouterModule, Routes, Router, ActivatedRoute }  from '@angular/router';
 
 @Component({
   selector: 'app-browse-offers-page',
@@ -23,6 +27,8 @@ export class BrowseOffersPageComponent implements OnInit {
   public user_id:number = null;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private api: TextbookTradeSystemApi
   ) { }
 
@@ -30,7 +36,11 @@ export class BrowseOffersPageComponent implements OnInit {
 
     let that = this;
 
-
+    this.getUserId().then (function (user_id:number) {
+      that.user_id = user_id;
+    }).catch (function (err) {
+      console.log(err);
+    })
 
     that.api.getOffers().then (function (offers: Offer[]) {
       that.offers = offers;
@@ -57,11 +67,45 @@ export class BrowseOffersPageComponent implements OnInit {
 
   //IMPLEMENTME
   sendOffer(offer_id:number) {
+    var new_pending_offer:PendingOffer = PendingOffer.createPendingOffer(offer_id, this.user_id);
+
+    let that = this;
+
+    that.api.createPendingOffer(new_pending_offer).then (function (pendingOffer: PendingOffer) {
+      alert(JSON.stringify(pendingOffer));
+      that.goToProfile();
+    }).catch (function (err) {
+      alert("Unable to send pending offer");
+      console.log(err);
+    })
 
   }
 
-  //IMPLEMENTME
+  goToProfile() {
+
+    this.router.navigate(['/profile'], {queryParams: {user_id: this.user_id}});
+
+  }
+
   getUserId() {
+
+    let that = this;
+
+    var user_id_promise = new Promise(function (resolve, reject) {
+      that.route.queryParams
+        .subscribe(params => {
+          let user_id:number = Number(params["user_id"]) || null;
+
+
+          if (user_id == null) {
+            reject("invalid_user");
+          }
+
+          resolve(user_id)
+        })
+    })
+
+    return user_id_promise;
 
   }
 
--- src/models/pendingoffer.ts
+++ src/models/pendingoffer.ts
@@ -7,6 +7,49 @@ export class PendingOffer {
 
   constructor() {}
 
+  /**
+    createEmptyPendingOffer()
+
+    ;; This method creates a empty pending offer as a skeleton
+
+  **/
+  static createEmptyPendingOffer() {
+
+    return <PendingOffer> {
+      id:null,
+      offer_id: null,
+      buyer_id: null
+    }
+
+  }
+
+  /**
+
+  offer_id: The id of the textbook offer
+  buyer_id: The id of the user who would like to purchase said offer with its benefits (textbook, advice, etc)
+
+  **/
+  static createPendingOffer(offer_id:number, buyer_id:number) {
+
+    var new_pending_offer:PendingOffer = PendingOffer.createEmptyPendingOffer();
+
+    new_pending_offer.offer_id = offer_id;
+    new_pending_offer.buyer_id = buyer_id;
+
+    return new_pending_offer;
+
+  }
+
+  /**
+
+  getPendingOfferPayload()
+
+  ;; This method parses the model to its respective payload form
+  ;; with the payload form (JSON in this case) the data can then be
+  ;; sent to the backend for storage and business logic in TextbookTradeSystemApi
+  
+  **/
+
   static getPendingOfferPayload(pendingoffer:PendingOffer) {
 
     return {
