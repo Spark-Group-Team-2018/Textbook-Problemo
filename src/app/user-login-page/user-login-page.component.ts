d3afdabbd8dd2121c40eb8747c9fbbc43153e5d0
Mechasparrow <mikipux7@gmail.com>
2018-04-03 21:04:24 -0500

    first user login test was success

--- src/app/user-login-page/user-login-page.component.ts
+++ src/app/user-login-page/user-login-page.component.ts
@@ -2,30 +2,49 @@ import { Component, OnInit } from '@angular/core';
 
 /** User Database System **/
 import {UserDatabase} from '../../lib/User_Database';
+import {User} from '../../models/user';
 
 /** Routing system **/
 import {Router, ActivatedRoute, ParamMap, NavigationExtras} from '@angular/router';
+
+/** Auth system **/
+import {AuthService} from '../providers/auth.service';
+
+//Auth lib
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-user-login-page',
   templateUrl: './user-login-page.component.html',
   styleUrls: ['./user-login-page.component.css'],
-  providers: [UserDatabase]
+  providers: [UserDatabase, AuthService, AngularFireAuth]
 })
 export class UserLoginPageComponent implements OnInit {
 
   constructor(
     private user_db: UserDatabase,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
 
   }
 
-  googleTestLogin() {
+  /** TODO add check logged in method **/
+  checkLoggedIn() {
 
+  }
+
+  googleTestLogin() {
+    this.authService.loginWithGoogle().then ((data) => {
+      alert ("Logged in!");
+      console.log(data);
+    }).catch ((err) => {
+      alert("ERRR");
+    })
   }
 
   userLogin(user_id:number) {
